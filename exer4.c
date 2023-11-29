62a86b598c2ff749fb644c67263524100f93517a
Davyconpdev <143567744+Davyconpdev@users.noreply.github.com>
2023-11-29 20:53:18 -0300

    Update exer4.c

--- exer4.c
+++ exer4.c
@@ -13,7 +13,7 @@ float somapar = 0, soma = 0, mediapar = 0, media = 0;
 
 for (i = 0; i < n; i++)
 {
-    printf("digite o %dº número:",i+1);
+    printf("Digite o %dÂº nÃºmero:",i+1);
     scanf("%d",&numero[i]);
     soma += numero[i];
     fflush(stdin);
@@ -38,12 +38,12 @@ for (i = 0; i < n; i++)
     media = soma / n;
 }
 system("cls || clear");
-printf("Quantidade de números impares: %d\n",impar);
-printf("Quantidade de números negativos: %d\n",negativo);
-printf("Maior número: %d\n",maiornumero);
-printf("Menor número: %d\n",menornumero);
-printf("Média dos números pares: %.1f\n",mediapar);
-printf("Média dos números inseridos: %.1f\n",media);
+printf("Quantidade de nÃºmeros impares: %d\n",impar);
+printf("Quantidade de nÃºmeros negativos: %d\n",negativo);
+printf("Maior nÃºmero: %d\n",maiornumero);
+printf("Menor nÃºmero: %d\n",menornumero);
+printf("MÃ©dia dos nÃºmeros pares: %.1f\n",mediapar);
+printf("MÃ©dia dos nÃºmeros inseridos: %.1f\n",media);
 
 
     
