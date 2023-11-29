15d1b99f53b10d4ea83779066105befb3320ac6d
Davyconpdev <143567744+Davyconpdev@users.noreply.github.com>
2023-11-29 20:50:14 -0300

    Add files via upload

--- exer5.c.c
+++ exer5.c.c
@@ -3,66 +3,66 @@
 #include <string.h>
 #include <locale.h>
 
-#define n 5
-#define MAX_LENGTH 370
+#define n 50
+#define TAM 370
 
 int main()
 {
     setlocale(LC_ALL, "portuguese");
 
-    char produto[n][MAX_LENGTH];
+    // Declaração de variáveis
+    char produto[n][TAM];
+    float preco[n];
     int quantidade[n];
-    float preco[n], valorfinal;
-    int i = 0;
+    float total[n];
     int opcao;
+    int i = 0, y;
 
+    // Solicitando dados
     do
     {
-        printf("\ncódigo |           opções            \n");
-        printf("\n  1   -   adicionar uma venda.        \n");
-        printf("\n  2   - exibir total de vendas e sair.\n");
+        printf("\nCódigo |         Descrição            \n");
+        printf("\n  1   -   Adicionar uma venda.        \n");
+        printf("\n  2   - Exibir total de vendas e sair.\n");
         printf("\n");
-        printf("\nescolha a opção desejada:");
+        printf("\nSeleciona a opção desejada:");
         scanf("%d", &opcao);
-
         system("cls || clear");
 
         switch (opcao)
         {
         case 1:
-            if (i < n)
+            fflush(stdin);
+            printf("\nNome do produto:");
+            gets(produto[i]);
+            printf("Valor(R$):");
+            scanf("%f", &preco[i]);
+            printf("Quantidade:");
+            scanf("%d", &quantidade[i]);
+            i++;
+            break;
+            system("cls || clear");
+
+        case 2:
+            system("cls || clear");
+
+            for (y = 0; y < i; y++)
             {
-                fflush(stdin);
-                printf("\nnome do produto:");
-                fgets(produto[i], MAX_LENGTH, stdin);
-                produto[i][strcspn(produto[i], "\n")] = '\0';
+                total[y] = preco[y] * quantidade[y];
 
-                printf("valor(R$):");
-                scanf("%f", &preco[i]);
-
-                printf("quantidade:");
-                scanf("%d", &quantidade[i]);
-                fflush(stdin);
-
-                i++;
-                valorfinal = preco[i] * quantidade[i];
-            
+                printf("\n%dº Produto: %s\n", y + 1, produto[y]);
+                printf("Preço: %.2f\n", preco[y]);
+                printf("Quantidade: %d\n", quantidade[y]);
+                printf("Total: %.2f\n", total[y]);
             }
+            break;
+
+        default:
+            printf("\nOpção inválida! Escolha novamente\n\n");
             break;
         }
 
     } while (opcao != 2);
-
-    for (i = 0; i < n; i++)
-    {
-        fflush(stdin);
-        printf("\n%dº produto: %s\n", i + 1, produto[i]);
-        printf("preço: %.2f\n", preco[i]);
-        printf("quantidade: %d\n", quantidade[i]);
-        printf("total: %.2f\n", valorfinal);
-    }
-    fflush(stdin);
-
 
     return 0;
 }
